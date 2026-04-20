0007e97a2e014e3d56f25203b8453e8c8603ce79
0xtaruhi <zhang_zhengyi@outlook.com>
2026-04-21 00:51:25 +0800

    fix: localize device color labels

--- src/components/virtual-device/DeviceInspector.vue
+++ src/components/virtual-device/DeviceInspector.vue
@@ -312,7 +312,7 @@ async function removeDevice() {
                 class="h-4 w-4 rounded-full border border-black/10"
                 :style="{ backgroundColor: color }"
               />
-              <span>{{ key }}</span>
+              <span>{{ t(key) }}</span>
             </button>
           </div>
         </section>
